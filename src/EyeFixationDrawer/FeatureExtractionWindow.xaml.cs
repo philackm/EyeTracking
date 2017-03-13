068e8aa7fa3471c9c04f55ae62c00e57454822f8
Phillip M <philmcka@icloud.com>
2017-03-13 14:42:17 +1100

    Add a few more dummy features to the feature extraction window

--- src/EyeFixationDrawer/FeatureExtractionWindow.xaml.cs
+++ src/EyeFixationDrawer/FeatureExtractionWindow.xaml.cs
@@ -57,9 +57,18 @@ namespace EyeFixationDrawer
         private void InitFeatureList()
         {
             List<FeatureExtractor> items = new List<FeatureExtractor>();
+
             items.Add(new FeatureExtractor() { featureName = "Fixation Duration (mean)", include = true, action = FixationDuration });
             items.Add(new FeatureExtractor() { featureName = "feature 2", include = true, action = (List<Fixation> fixations) => 2 });
             items.Add(new FeatureExtractor() { featureName = "feature 3", include = false, action = (List<Fixation> fixations) => 3 });
+            items.Add(new FeatureExtractor() { featureName = "feature 4", include = true, action = (List<Fixation> fixations) => 4 });
+            items.Add(new FeatureExtractor() { featureName = "feature 5", include = false, action = (List<Fixation> fixations) => 5 });
+            items.Add(new FeatureExtractor() { featureName = "feature 6", include = true, action = (List<Fixation> fixations) => 6 });
+            items.Add(new FeatureExtractor() { featureName = "feature 7", include = false, action = (List<Fixation> fixations) => 7 });
+            items.Add(new FeatureExtractor() { featureName = "feature 8", include = true, action = (List<Fixation> fixations) => 8 });
+            items.Add(new FeatureExtractor() { featureName = "feature 9", include = false, action = (List<Fixation> fixations) => 9 });
+            items.Add(new FeatureExtractor() { featureName = "feature 10", include = true, action = (List<Fixation> fixations) => 10 });
+            items.Add(new FeatureExtractor() { featureName = "feature 11", include = false, action = (List<Fixation> fixations) => 11 });
 
             featureList.ItemsSource = items;
         }
